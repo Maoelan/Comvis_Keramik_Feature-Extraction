e758632ff6c5ba0fd28eb664ee31dfefded2080b
maoelan <maulanamuhammad2000@gmail.com>
2022-05-26 11:00:07 +0700

    tugas comvis

---
+++ tugas_ekstraksi_citra.m
@@ -0,0 +1,192 @@
+function varargout = tugas_ekstraksi_citra(varargin)
+% TUGAS_EKSTRAKSI_CITRA MATLAB code for tugas_ekstraksi_citra.fig
+%      TUGAS_EKSTRAKSI_CITRA, by itself, creates a new TUGAS_EKSTRAKSI_CITRA or raises the existing
+%      singleton*.
+%
+%      H = TUGAS_EKSTRAKSI_CITRA returns the handle to a new TUGAS_EKSTRAKSI_CITRA or the handle to
+%      the existing singleton*.
+%
+%      TUGAS_EKSTRAKSI_CITRA('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TUGAS_EKSTRAKSI_CITRA.M with the given input arguments.
+%
+%      TUGAS_EKSTRAKSI_CITRA('Property','Value',...) creates a new TUGAS_EKSTRAKSI_CITRA or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before tugas_ekstraksi_citra_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to tugas_ekstraksi_citra_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help tugas_ekstraksi_citra
+
+% Last Modified by GUIDE v2.5 26-May-2022 10:21:32
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @tugas_ekstraksi_citra_OpeningFcn, ...
+                   'gui_OutputFcn',  @tugas_ekstraksi_citra_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before tugas_ekstraksi_citra is made visible.
+function tugas_ekstraksi_citra_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to tugas_ekstraksi_citra (see VARARGIN)
+
+% Choose default command line output for tugas_ekstraksi_citra
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes tugas_ekstraksi_citra wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = tugas_ekstraksi_citra_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[filename,pathname] = uigetfile({'*.*'});
+ 
+if ~isequal(filename,0)
+    Info = imfinfo(fullfile(pathname,filename));
+    if Info.BitDepth == 1
+        msgbox('Citra masukan harus citra RGB atau Grayscale');
+        return
+    elseif Info.BitDepth == 8
+        Img = imread(fullfile(pathname,filename));
+        axes(handles.axes1)
+        cla('reset')
+        imshow(Img)
+        title('Grayscale Image')
+    else
+        Img = rgb2gray(imread(fullfile(pathname,filename)));
+        axes(handles.axes1)
+        cla('reset')
+        imshow(Img)
+        title('Grayscale Image')
+    end
+else
+    return
+end
+ 
+handles.Img = Img;
+guidata(hObject,handles);
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+Img = handles.Img;
+pixel_dist = str2double(get(handles.edit1,'String'));
+GLCM = graycomatrix(Img,'Offset',[0 pixel_dist; -pixel_dist pixel_dist; -pixel_dist 0; -pixel_dist -pixel_dist]);
+stats = graycoprops(GLCM,{'contrast','correlation','energy','homogeneity'});
+ 
+Contrast = stats.Contrast;
+Correlation = stats.Correlation;
+Energy = stats.Energy;
+Homogeneity = stats.Homogeneity;
+
+jumlah = (mean(Homogeneity)+mean(Energy))/2;
+set(handles.text4,'string',jumlah);
+if (jumlah < 0.59)
+    set (handles.text5, 'string', 'Permukaan Kasar');
+end
+if (jumlah >= 0.59)
+    set (handles.text5, 'string', 'Permukaan Halus');
+end;
+ 
+data = get(handles.uitable1,'Data');
+data{1,1} = num2str(Contrast(1));
+data{1,2} = num2str(Contrast(2));
+data{1,3} = num2str(Contrast(3));
+data{1,4} = num2str(Contrast(4));
+data{1,5} = num2str(mean(Contrast));
+ 
+data{2,1} = num2str(Correlation(1));
+data{2,2} = num2str(Correlation(2));
+data{2,3} = num2str(Correlation(3));
+data{2,4} = num2str(Correlation(4));
+data{2,5} = num2str(mean(Correlation));
+ 
+data{3,1} = num2str(Energy(1));
+data{3,2} = num2str(Energy(2));
+data{3,3} = num2str(Energy(3));
+data{3,4} = num2str(Energy(4));
+data{3,5} = num2str(mean(Energy));
+ 
+data{4,1} = num2str(Homogeneity(1));
+data{4,2} = num2str(Homogeneity(2));
+data{4,3} = num2str(Homogeneity(3));
+data{4,4} = num2str(Homogeneity(4));
+data{4,5} = num2str(mean(Homogeneity));
+ 
+set(handles.uitable1,'Data',data,'ForegroundColor',[0 0 0])
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes when entered data in editable cell(s) in uitable1.
+function uitable1_CellEditCallback(hObject, eventdata, handles)
+% hObject    handle to uitable1 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Indices: row and column indices of the cell(s) edited
+%	PreviousData: previous data for the cell(s) edited
+%	EditData: string(s) entered by the user
+%	NewData: EditData or its converted form set on the Data property. Empty if Data was not changed
+%	Error: error string when failed to convert EditData to appropriate value for Data
+% handles    structure with handles and user data (see GUIDATA)
